f70865a31efd22a074683d38f2cab3e97b4bf804
Nahuel Maniaci <nahuelmaniaci@gmail.com>
2024-12-30 22:39:42 -0300

    passss

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const express = require('express');
-const bcrypt = require('bcrypt');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
-const USERNAME = process.env.USERNAME; // Nombre de usuario
-const PASSWORD = process.env.PASSWORD; // Contraseña en hash
+// Credenciales fijas
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
 
 router.post('/login', async (req, res) => {
-    console.log('Cuerpo de la solicitud:', req.body);
+    console.log('Cuerpo de la solicitud:', req.body); // Log para verificar el cuerpo recibido
     const { username, password } = req.body;
 
     if (!username || !password) {
@@ -15,12 +16,10 @@ router.post('/login', async (req, res) => {
         return res.status(400).json({ error: 'Faltan credenciales' });
     }
 
-    // Comparar contraseña con el hash almacenado
     const isPasswordValid = await bcrypt.compare(password, PASSWORD);
-    
     if (username === USERNAME && isPasswordValid) {
         console.log('Login exitoso:', { username });
-        return res.status(200).json({ message: 'Login exitoso' });
+        return res.status(200).json({ message: 'Login exitoso' }); // Respuesta de éxito
     } else {
         console.error('Credenciales inválidas:', { username });
         return res.status(401).json({ error: 'Credenciales inválidas' });
